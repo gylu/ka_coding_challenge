ffe489914489613c14a11cfe63a6fdeb35495dd6
George Lu <gylu88@gmail.com>
2016-11-07 20:03:50 -0800

    total_infection works

--- webserver/app/views.py
+++ webserver/app/views.py
@@ -12,6 +12,35 @@ import string
 
 DATABASE = '../ka_challenge.db' #assuming we are running from coding_challenge/webserver/run.py, and ka_challenge.db is at coding_challenge/users.db
 
+@app.route('/_view_database')
+def view_database():
+    return jsonify(count_infection_versions())
+
+@app.route('/_total_infection')
+def total_infection():
+    infection_version=request.args.get('infection_version', 0, type=str)
+    print("infection_version", infection_version)
+    if not isinstance(infection_version, str):
+        print("performing total_infection")
+        infection_version=1.1
+    cur = g.db.cursor()
+    query = 'update users set version='+str(infection_version)
+    cur.execute(query)
+    g.db.commit()
+    cur.close()
+    return jsonify(count_infection_versions())
+
+def count_infection_versions():
+    total_num_users = g.db.execute("SELECT count(*) FROM USERS;").fetchall()
+    num_of_distinct_versions = g.db.execute("SELECT count(distinct(version)) FROM users").fetchall()[0][0]
+    list_of_versions = [version_item[0] for version_item in g.db.execute("SELECT distinct(version) FROM users").fetchall()] #because for some reason, the query returns with: [(1.1,), (1,)]
+    ten_random_users = g.db.execute("SELECT * FROM USERS LIMIT 10;").fetchall()
+    users_per_version=[]
+    for version in list_of_versions:
+        users_per_version.append(g.db.execute("SELECT count(*) FROM users where version="+str(version)).fetchall()[0][0])
+    versions_info={"total_num_users":total_num_users,"num_of_distinct_versions":num_of_distinct_versions, "list_of_versions":list_of_versions, "users_per_version":users_per_version, "ten_random_users":ten_random_users}
+    print("versions_info:",versions_info)
+    return versions_info
 
 @app.route('/_populate_database')
 def populate_database():
@@ -20,21 +49,8 @@ def populate_database():
     if num_entries==0 or not isinstance(num_entries, int):
         print("here")
         num_entries=100
-    count=0
-    cur = g.db.cursor()
-    while count < num_entries:
-        userName=random.choice(string.ascii_letters[26::])+random.choice(['a','e','i','o','u','y'])+random.choice(string.ascii_letters[0:26])+random.choice(string.ascii_letters[0:26])+random.choice(['a','e','i','o','u','y'])
-        # insert_result = insert("USERS", ['name','version'], [userName,1.0])
-        query = getquery("USERS", ['name','version'], [userName,1.0])
-        count+=1
-        cur.execute(query, [userName,1.0])
-    g.db.commit()
-    id = cur.lastrowid
-    cur.close()
-    users = g.db.execute("SELECT * FROM USERS LIMIT 10;").fetchall()
-    count = g.db.execute("SELECT count(*) FROM USERS WHERE version=1.0;").fetchall()        
-    response_data=jsonify(users=users,counts=count)
-    return response_data
+    fast_db_populate(num_entries)
+    return jsonify(count_infection_versions())
 
 
 def fast_db_populate(num_entries):
@@ -49,7 +65,6 @@ def fast_db_populate(num_entries):
     g.db.commit()
     id = cur.lastrowid
     cur.close()
-    return True
 
 def getquery(table, fields=(), values=()):
     query = 'INSERT INTO %s (%s) VALUES (%s)' % (
@@ -75,15 +90,6 @@ def insert(table, fields=(), values=()):
     return id
 
 
-
-
-@app.route('/_view_database')
-def view_database():
-    print("g.db", g.db)
-    users = g.db.execute("SELECT * FROM USERS LIMIT 10;").fetchall()
-    count = g.db.execute("SELECT count(*) FROM USERS WHERE version=1.0;").fetchall()
-    response_data=jsonify(users=users,counts=count)
-    return response_data
 
 
 @app.before_request
